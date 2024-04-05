8282169abad7550ee45966753e8a40bad46ef0a5
acaj0 <antoniocarlosjr007@hotmail.com>
2024-04-05 15:13:37 -0400

    NextAuth Configuration

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,38 @@
+import type { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "your-cool-username",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        const user = { id: "42", name: "dave", password: "nextauth" };
+
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
